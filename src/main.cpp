10050e2d6ba1667577683643bf91c82761da5fe0
Bushstar <bushsolo@gmail.com>
2014-06-09 09:58:44 +0100

    Restore original premine amount

--- src/main.cpp
+++ src/main.cpp
@@ -1087,7 +1087,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = 5000 * COIN;
     if (nHeight == 1)
     {
-        return nSubsidy = 40000000 * COIN;
+        return nSubsidy = 60000000 * COIN;
     }
 	
 	// Subsidy is cut in half every 400000 blocks, which will occur approximately every 4 years
