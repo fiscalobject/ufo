5c527b13763e665f74f635488b0282c5f5e17a94
Bushstar <bushsolo@gmail.com>
2014-06-09 20:47:14 +0100

    Update comment on subsidy halving

--- src/main.cpp
+++ src/main.cpp
@@ -1090,8 +1090,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
         return nSubsidy = 40000000 * COIN;
     }
 	
-	// Subsidy is cut in half every 400000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 400000); // UFO Coin: 400000 approximately every 4 years
+	// Subsidy is cut in half every 416 days
+    nSubsidy >>= (nHeight / 400000);
 	
     return nSubsidy + nFees;
 }
