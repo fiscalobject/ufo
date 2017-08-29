ea256b8f17813828139a98afe32de14909ea9613
Peter Bushnell <bushsolo@gmail.com>
2017-08-29 10:06:21 +0100

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# Copyright (c) 2013-2016 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
-autoreconf --install --force
+which autoreconf >/dev/null || \
+  (echo "configuration failed, please install autoconf first" && exit 1)
+autoreconf --install --force --warnings=all
