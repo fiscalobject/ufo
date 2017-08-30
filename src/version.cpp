a315769df4822933d4cd7555e32eee007bb0049c
Peter Bushnell <bushsolo@gmail.com>
2017-08-30 20:55:20 +0100

    Remove -unk from build

--- src/version.cpp
+++ src/version.cpp
@@ -47,7 +47,7 @@ const std::string CLIENT_NAME("Satoshi");
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build)
 
 #ifndef BUILD_DESC
 #    ifdef BUILD_SUFFIX
