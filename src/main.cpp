cb3835c8fd909183f78e2901191167ff480be078
Peter Bushnell <bushsolo@gmail.com>
2017-10-02 12:11:42 +0100

    Set block subsidy

--- src/main.cpp
+++ src/main.cpp
@@ -1233,14 +1233,16 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex)
 
 CAmount GetBlockValue(int nHeight, const CAmount& nFees)
 {
-    CAmount nSubsidy = 50 * COIN;
+    int64_t nSubsidy = 5000 * COIN;
     int halvings = nHeight / Params().SubsidyHalvingInterval();
+
+    if (nHeight == 1)
+        return nSubsidy = 40000000 * COIN;
 
     // Force block reward to zero when right shift is undefined.
     if (halvings >= 64)
         return nFees;
 
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
 
     return nSubsidy + nFees;
