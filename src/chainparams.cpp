09c47d852581513d3880a1855a13952de000c135
Peter Bushnell <bushsolo@gmail.com>
2018-01-06 07:33:41 +0000

    Lower testnet fork

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -135,7 +135,7 @@ public:
         consensus.nHardForkTwoA = 1;
         consensus.nHardForkThree = 1;
         consensus.nHardForkFour = 1100;
-        consensus.nHardForkFourA = 1500;
+        consensus.nHardForkFourA = 1200;
         consensus.nNeoScryptSwitch = 1506816000;
         consensus.nNeoScryptFork = 1506816000;
         consensus.fPowAllowMinDifficultyBlocks = true;
