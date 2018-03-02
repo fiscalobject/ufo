72ddfd7d8368b61395a7ce653f99ed6bd18840f9
Peter Bushnell <bushsolo@gmail.com>
2018-03-02 11:44:56 +0000

    GUI/RPC rename

--- src/init.cpp
+++ src/init.cpp
@@ -777,7 +777,7 @@ void InitLogging()
     fLogIPs = GetBoolArg("-logips", DEFAULT_LOGIPS);
 
     LogPrintf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    LogPrintf("Bitcoin version %s\n", FormatFullVersion());
+    LogPrintf("UFO version %s\n", FormatFullVersion());
 }
 
 /** Initialize bitcoin.
