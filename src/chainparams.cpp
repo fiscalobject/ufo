8504b25b650a39556454a7d0a67f3be31ad64ef5
Peter Bushnell <bushsolo@gmail.com>
2018-04-11 09:20:26 +0100

    eHRC

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -72,6 +72,8 @@ public:
         consensus.nHardForkTwo = 160997;
         consensus.nHardForkTwoA = 171900;
         consensus.nHardForkThree = 266000;
+        consensus.nHardForkFour = 1182000;
+        consensus.nHardForkFourA = 1220000;
         consensus.nNeoScryptSwitch = 1414195200;
         consensus.nNeoScryptFork = 1414446393;
         consensus.fPowAllowMinDifficultyBlocks = false;
@@ -175,6 +177,8 @@ public:
         consensus.nHardForkTwo = 1;
         consensus.nHardForkTwoA = 1;
         consensus.nHardForkThree = 1;
+        consensus.nHardForkFour = 1100;
+        consensus.nHardForkFourA = 1500;
         consensus.nNeoScryptSwitch = 1506816000;
         consensus.nNeoScryptFork = 1506816000;
         consensus.fPowAllowMinDifficultyBlocks = true;
