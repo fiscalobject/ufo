294e7fdfbedeb95387dee5eadc18bf2114dcd0d3
Peter Bushnell <bushsolo@gmail.com>
2018-05-18 10:49:36 +0100

    BTC > UFO

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("µBTC (bits)");
+    case BTC: return QString("UFO");
+    case mBTC: return QString("mUFO");
+    case uBTC: return QString::fromUtf8("µUFO (bits)");
     default: return QString("???");
     }
 }
@@ -60,9 +60,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("UFOs");
+    case mBTC: return QString("Milli-UFOs (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-UFOs (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
