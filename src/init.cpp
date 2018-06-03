35b3b358a8f48b77359cbad15af85a4ca6e780cc
Peter Bushnell <bushsolo@gmail.com>
2018-06-03 10:10:04 +0100

    New GitHub repo

--- src/init.cpp
+++ src/init.cpp
@@ -522,7 +522,7 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/UFOCoins/ufo>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/fiscalobject/ufo>";
     const std::string URL_WEBSITE = "<https://ufobject.com>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2014, COPYRIGHT_YEAR) + " ") + "\n" +
