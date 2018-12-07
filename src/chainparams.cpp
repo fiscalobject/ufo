b9d4d99cd4f4482ddd75835b92507c862e8304f7
Peter Bushnell <bushsolo@gmail.com>
2018-12-07 07:28:21 +0000

    Set correct private key

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -136,7 +136,7 @@ public:
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,27);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,68);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,68);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,155);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
